d7c8d7594ea83c9726fb4b1b59b6ee9a8b59d8d6
remiranda <pesca_botoes.0t@icloud.com>
2023-06-27 18:26:28 -0300

    norminette

--- include/defines.h
+++ include/defines.h
@@ -6,14 +6,14 @@
 /*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 10:05:56 by rmiranda          #+#    #+#             */
-/*   Updated: 2023/06/27 17:50:33 by rmiranda         ###   ########.fr       */
+/*   Updated: 2023/06/27 18:26:04 by rmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFINES_H
 # define DEFINES_H
 
-typedef struct	s_philo_args {
+typedef struct s_philo_args {
 	int	number_of_philosophers;
 	int	time_to_die;
 	int	time_to_eat;
@@ -22,11 +22,11 @@ typedef struct	s_philo_args {
 	int	number_of_times_each_philosopher_must_eat;
 }	t_philo_args;
 
-typedef struct	s_philo_info {
+typedef struct s_philo_info {
 	t_philo_args	args;
 	pthread_mutex_t	print_mutex;
 	pthread_mutex_t	*forks;
-	pthread_t	*tdata;
+	pthread_t		*tdata;
 }	t_philo_info;
 
 #endif
