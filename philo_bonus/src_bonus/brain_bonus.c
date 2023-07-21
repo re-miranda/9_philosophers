5639fcc28dbf15d50c178254ca7cc275f72aefe9
remiranda <pesca_botoes.0t@icloud.com>
2023-07-21 09:13:49 +0000

    norminette

--- philo_bonus/src_bonus/brain_bonus.c
+++ philo_bonus/src_bonus/brain_bonus.c
@@ -6,25 +6,25 @@
 /*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 10:05:52 by rmiranda          #+#    #+#             */
-/*   Updated: 2023/07/20 23:55:41 by rmiranda         ###   ########.fr       */
+/*   Updated: 2023/07/21 06:13:04 by rmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bonus.h>
 
-int	philo_brain(t_philo_info *info_ptr, int philo_id)
+int	philo_brain(t_philo_info *info_ptr, int id)
 {
-	info_ptr->health_data[philo_id].meal_tv.tv_sec = info_ptr->start_tv.tv_sec;
-	info_ptr->health_data[philo_id].meal_tv.tv_usec = info_ptr->start_tv.tv_usec;
-	if (philo_id % 2)
+	info_ptr->health_data[id].meal_tv.tv_sec = info_ptr->start_tv.tv_sec;
+	info_ptr->health_data[id].meal_tv.tv_usec = info_ptr->start_tv.tv_usec;
+	if (id % 2)
 		usleep(5000);
-	while (info_ptr->health_data[philo_id].meal_count != 0)
+	while (info_ptr->health_data[id].meal_count != 0)
 	{
-		philo_think(info_ptr, philo_id);
-		philo_eat(info_ptr, philo_id);
-		if (set_meal_tv(info_ptr, philo_id))
+		philo_think(info_ptr, id);
+		philo_eat(info_ptr, id);
+		if (set_meal_tv(info_ptr, id))
 			return (-1);
-		philo_sleep(info_ptr, philo_id);
+		philo_sleep(info_ptr, id);
 	}
 	return (0);
 }
--- philo_bonus/src_bonus/simulation_helper_bonus.c
+++ philo_bonus/src_bonus/simulation_helper_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 23:35:19 by rmiranda          #+#    #+#             */
-/*   Updated: 2023/07/21 11:07:43 by coder            ###   ########.fr       */
+/*   Updated: 2023/07/21 06:13:26 by rmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,13 @@ void	interrupt_simulation(t_philo_info *info_ptr, int philo_id, t_tv tv)
 	sem_wait(info_ptr->print_sem);
 	sem_wait(info_ptr->simulation_sem);
 	printf("%ld %i died\n", e_time, philo_id);
+}
+
+int	child_destroy(t_philo_info *info_ptr, int return_value)
+{
+	(void)info_ptr;
+	sem_unlink("/print_sem");
+	sem_unlink("/simulation_sem");
+	sem_unlink("/forks_sem");
+	exit (return_value);
 }
